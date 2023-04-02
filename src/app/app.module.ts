903414d52d3c11212507a659926468ed14b16da8
Ronald <rl8506@gmail.com>
2023-04-02 18:30:09 -0300

    variables y directivas para inicializaras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Ejemplo2Component } from './ejemplo2/ejemplo2.component';
 import { Ejemplo3Component } from './ejemplos/ejemplo3/ejemplo3.component';
 import { Ejemplo4Component } from './ejemplos/ejemplo4/ejemplo4.component';
+import { BasicosComponent } from './ejemplos/basicos/basicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Ejemplo2Component,
     Ejemplo3Component,
-    Ejemplo4Component
+    Ejemplo4Component,
+    BasicosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ejemplos/basicos/basicos.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styleUrls: ['./basicos.component.css']
+})
+export class BasicosComponent {
+  nombre? : string;
+  nombreMio : string = "Ronald lima";
+  correo : any = "rli@gmail.com";
+  edad:number = 43;
+  fecha = new Date();
+  html : string = `<strong>texto en negritas</strong>`;
+  paises : Array<any>;
+  ejemplo : string;
+  constructor(){
+    this.nombre = "Ridchel"
+    this,this.paises = [
+      {
+        nombre: "Chile",
+        dominio: "cl"
+      },
+      {
+        nombre: "Uruguay",
+        dominio: "uy"
+      },
+      {
+        nombre: "Argentina",
+        dominio: "ar"
+      },
+    ];
+  }
+}

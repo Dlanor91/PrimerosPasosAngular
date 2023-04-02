aa050a6a6405661674fec7c9e2e447ba7319cb30
Ronald <rl8506@gmail.com>
2023-04-02 17:54:32 -0300

    crearComponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ejemplo2Component } from './ejemplo2/ejemplo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ejemplo2Component
   ],
   imports: [
     BrowserModule,

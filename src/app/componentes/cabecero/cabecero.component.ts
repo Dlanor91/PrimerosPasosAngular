15b9256eb3bb312640bf74c563d075a304925c2e
Ronald <rl8506@gmail.com>
2023-04-22 20:37:14 -0300

    cerrar sesion

--- src/app/componentes/cabecero/cabecero.component.ts
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-cabecero',
   templateUrl: './cabecero.component.html',
   styleUrls: ['./cabecero.component.css']
 })
-export class CabeceroComponent {
+export class CabeceroComponent implements OnInit{
+  boleano : boolean;
+  constructor(private cookieService:CookieService){
+  }
 
+  ngOnInit(): void {
+    if(this.cookieService.check('var_login')){
+      this.boleano = true;
+    }else{
+      this.boleano = false;
+    }
+  }
 }
--- src/app/paginas/acceso-restringido/acceso-restringido.component.ts
+++ src/app/paginas/acceso-restringido/acceso-restringido.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-acceso-restringido',
   templateUrl: './acceso-restringido.component.html',
   styleUrls: ['./acceso-restringido.component.css']
 })
-export class AccesoRestringidoComponent {
+export class AccesoRestringidoComponent implements OnInit{
+  nombre:string;
+  constructor(private cookieService:CookieService, private router: Router){
 
+  }
+
+  ngOnInit(): void {
+    this.nombre = this.cookieService.get('var_nombre');
+  }
+
+  salir(){
+    this.cookieService.deleteAll();
+    swal.fire({
+      position:'top-end',
+      timer:5000,
+      icon:'success',
+      title:'Ok',
+      text:"Cerraste sesión exitosamente."
+    });
+    this.router.navigate(['/login'])
+      .then(() => {
+        window.location.reload();
+      });
+  }
 }
